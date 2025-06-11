23e0714eea547119b6017c80f05f2bad0b9dc66c
paung29 <aikepaung767@gmail.com>
2025-06-12 02:53:47 +0700

    add

--- rew-05-state-management/src/pages/structure/flat-structure-state.tsx
+++ rew-05-state-management/src/pages/structure/flat-structure-state.tsx
@@ -7,7 +7,7 @@ export default function FlatStructure (){
     const basePlan = flatPlan[0]
     const [selectedPlan, setSelectedPlan] = useState<number>(1)
 
-    const handleVisit = (placeId : number, parendId : number) => {
+    const handleOnVisit = (placeId : number, parendId : number) => {
         const parent = flatPlan[parendId]
         const nextParent : FlatPlan = {...parent, childIds : parent.childIds.filter(id => id != placeId)}
         flatPlan[parendId] = nextParent
@@ -17,6 +17,17 @@ export default function FlatStructure (){
     return (
         <>
             <h3>Flat State</h3>
+
+            <section className="accordion">
+                {basePlan.childIds.map(childId => <PlaceComponent
+                        key={childId}
+                        planId={childId}
+                        parentId={0}
+                        planRecord={flatPlan}
+                        selectetId={selectedPlan}
+                        onVisit={handleOnVisit}
+                        onClick={setSelectedPlan} />)}
+            </section>
         </>
     )
 }
@@ -27,24 +38,43 @@ function PlaceComponent ({planId, parentId, planRecord, selectetId, onVisit, onC
     planRecord : Record<number, FlatPlan>,
     selectetId : number,
     onVisit : (placeId : number, parendId : number) => void,
-    onClick : ()=> void})
+    onClick : (placeId : number) => void})
 
 {
 
     const plan = planRecord[planId]
     const show = (planId == selectetId || hasChildForShow(planRecord, planId, selectetId))
 
-
+    if(plan.childIds.length == 0){
+        return(
+            <div className="card mb-2">
+                <div className="card-body d-flex justify-content-between">
+                    <span>{plan.title}</span>
+                    <button onClick={() =>  onVisit(planId, parentId)} type="button" className="btn btn-outline-primary">
+                        <i className="bi-chekck"></i>Visited
+                    </button>
+                </div>
+            </div>
+        )
+    }
 
     return(
         <div className="accordion-item">
-            <h5 className="accordion-header">
-                <button>{plan.title}</button>
+            <h5 className="accordion-header" onClick={() => onClick(planId )}>
+                <button className={`accordion-button ${show ? '': 'collapsed'}`} data-bs-toogle="collapse" data-bs-target={`#collapse-${planId}`}>{plan.title}</button>
             </h5>
 
-            <div className="">
+            <div id={`collapse-${plan.id}`} className={`accordion-collapse collapse ${show ? 'show' : ''}`}>
                 <div className="accordion-body">
-                    
+                    {plan.childIds.map(childId => <PlaceComponent
+                        key={childId}
+                        planId={childId}
+                        parentId={planId}
+                        planRecord={planRecord}
+                        selectetId={selectetId}
+                        onVisit={onVisit}
+                        onClick={onClick}
+                     />)}
                 </div>
             </div>
         </div>
