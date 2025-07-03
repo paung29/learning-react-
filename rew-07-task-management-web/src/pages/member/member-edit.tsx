508447724fa98b9ac0a5c2bc8703bbd2fde03806
paung29 <aikepaung767@gmail.com>
2025-07-03 21:00:44 +0700

    update

--- rew-07-task-management-web/src/pages/member/member-edit.tsx
+++ rew-07-task-management-web/src/pages/member/member-edit.tsx
@@ -1,6 +1,9 @@
 import { useForm } from "react-hook-form"
 import type { MemberEditForm } from "../../model/input/member-edit-form"
 import { useNavigate, useSearchParams } from "react-router"
+import Page from "../../ui/page"
+import { useEffect } from "react"
+import { findMemberEditForm } from "../../model/client/member-client"
 
 export default function MemberEdit(){
 
@@ -9,8 +12,18 @@ export default function MemberEdit(){
     const [queryParams] = useSearchParams()
     const navigate = useNavigate()
 
+    useEffect(() => {
+        async function load(id : string) {
+            const result = await findMemberEditForm(id)
+            reset(result)
+        }
+    })
+
     return(
         <>
+            <Page title="Member Edit" icon="bi-pencil">
+                <div></div>
+            </Page>
         </>
     )
 }
