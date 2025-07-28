9b986b85042ba8592e67af752947c2e060c1e62b
paung29 <aikepaung767@gmail.com>
2025-07-28 16:30:22 +0700

    update

--- rew-08-task-management-web/src/App.tsx
+++ rew-08-task-management-web/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AccordionHeader } from "@radix-ui/react-accordion";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./components/ui/accordion";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
+  return(
+    <main className="mt-8 mx-16">
+      
+      <h1 className="text-4xl">Moe Shar</h1>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <Accordion type="single" collapsible defaultValue="acc-1">
+        {Array.from({length : 4}).map((_, index) => 
+          <AccordionItem key={index} value={`acc-${index + 1}`}>
+            <AccordionTrigger>
+              <AccordionHeader>{`Accordion ${index +1}`}</AccordionHeader>
+            </AccordionTrigger>
+
+            <AccordionContent>
+              {`Content of Accordion ${index + 1}`}
+            </AccordionContent>
+          </AccordionItem>
+        )}
+      </Accordion>
+
+    </main>
   )
 }
-
-export default App
