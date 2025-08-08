6ff5a8ffa45f00a2ac9deb1335c12a59d1ff95b6
paung29 <aikepaung767@gmail.com>
2025-08-08 13:41:13 +0700

    update

--- rew-08-task-management-web/src/components/pages/tasks/task-list.tsx
+++ rew-08-task-management-web/src/components/pages/tasks/task-list.tsx
@@ -1,10 +1,16 @@
+import ButtonWrapper from "@/components/custom/button-wrapper";
+import FormGroup from "@/components/custom/form-group";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { searchTasks } from "@/lib/client/task-client";
 import type { TaskListItem } from "@/lib/model/output/task-list-item";
 import { TaskSearchSchema, type TaskSearch } from "@/lib/model/schema/task-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowRight, Plus, Search } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 
 export default function TaskList(){
 
@@ -37,6 +43,68 @@ function SearchForm({search} : {search : (form : TaskSearch) => void}){
     },[id, resetField])
 
     return(
-        <></>
+        <form ref={formRef} onSubmit={handleSubmit(search)} className="flex gap-2">
+            <input type="hidden" {...register('projectId')} />
+
+            <FormGroup label="Start From">
+                    <Input {...register('startFrom')} type="date"/>
+            </FormGroup>
+
+            <FormGroup label="Start To">
+                    <Input {...register('startTo')} type="date"/>
+            </FormGroup>
+
+            <FormGroup label="Keyword">
+                    <Input {...register('keyword')} placeholder="Search Keyword"/>
+            </FormGroup>
+
+            <ButtonWrapper>
+                <Button type="submit">
+                    <Search /> Search
+                </Button>
+
+                <Button asChild className="ms-1">
+                    <Link to={`/project/${id}/task/edit`}>
+                        <Plus /> Create Task
+                    </Link>
+                </Button>
+            </ButtonWrapper>
+        </form>
+    )
+}
+
+function ResultTable({list} : {list : TaskListItem[]}){
+    const {id} = useParams()
+
+    return(
+        <Table >
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Assignee</TableHead>
+                    <TableHead>Due Date</TableHead>
+                    <TableHead>Start Date</TableHead>
+                    <TableHead>End Date</TableHead>
+                    <TableHead>Name</TableHead>
+                </TableRow>
+            </TableHeader>
+
+            <TableBody>
+                {list.map(item => 
+                    <TableRow key={item.id}>
+                        <TableCell>{item.name}</TableCell>
+                        <TableCell>{item.assignee}</TableCell>
+                        <TableCell>{item.dueDate}</TableCell>
+                        <TableCell>{item.startDate}</TableCell>
+                        <TableCell>{item.endDate}</TableCell>
+                        <TableCell>
+                            <Link to={`/project/${id}/task/${item.id}`}>
+                                <ArrowRight />
+                            </Link>
+                        </TableCell>
+                    </TableRow>
+                )}
+            </TableBody>
+        </Table>
     )
 }
