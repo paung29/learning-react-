2d263c6b85e53076590a9c2da6c3ef2e7dd21ad0
paung29 <aikepaung767@gmail.com>
2025-08-12 13:35:07 +0700

    udpate

--- rew-09-learning-forms/src/App.tsx
+++ rew-09-learning-forms/src/App.tsx
@@ -1,9 +1,22 @@
-import { Calendar, CheckCircle, CheckSquare, File, Files, Group, Image, ListCheck, PenBox, Settings, type LucideProps } from "lucide-react"
+import { Calendar, CheckCircle, CheckSquare, ChevronDown, File, Files, Group, House, Image, ListCheck, PenBox, Settings, type LucideProps } from "lucide-react"
 import type React from "react"
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger } from "./components/ui/sidebar"
+import { Link, Outlet } from "react-router"
+import { useState } from "react"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./components/ui/collapsible"
 
 export default function App(){
   return(
-    <></>
+    <SidebarProvider>
+      <AppSideBar />
+        <div>
+          <SidebarTrigger></SidebarTrigger>
+
+          <main>
+            <Outlet/>
+          </main>
+        </div>
+    </SidebarProvider>
   )
 }
 
@@ -47,7 +60,52 @@ const MENU: MenuGroup[] = [
 ]
 
 function AppSideBar() {
+
+  const [selected, setSelected] = useState(0)
+
   return(
-    <></>
+    <Sidebar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link to={``}><House/> Using Forms</Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarContent>
+        {MENU.map((group, groupIndex)=> 
+          <Collapsible key={`group-${groupIndex + 1}`} open={selected == groupIndex} className="group/collapsible">
+            <SidebarGroup>
+              <SidebarGroupLabel onClick={() => setSelected(groupIndex)} asChild>
+                <CollapsibleTrigger>
+                  {group.label}
+                  <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180"/>
+                </CollapsibleTrigger>
+              </SidebarGroupLabel>
+
+              <CollapsibleContent>
+                <SidebarGroupContent>
+                  <SidebarMenu>
+                    {group.items.map((item, itemIndex) =>
+                      <SidebarMenuItem key={`item-${groupIndex +1}-${itemIndex +1}`}>
+                        <SidebarMenuButton asChild>
+                          <Link to={item.url} >
+                            {item.icon && <item.icon/>}
+                            <span>{item.title}</span>
+                          </Link>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem> 
+                    )}
+                  </SidebarMenu>
+                </SidebarGroupContent>
+              </CollapsibleContent>
+            </SidebarGroup>
+          </Collapsible>
+        )}
+      </SidebarContent>
+    </Sidebar>
   )
 }
