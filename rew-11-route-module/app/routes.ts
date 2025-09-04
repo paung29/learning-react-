e398e5a7c3ef54d5922a556955a0e236cd019b0f
paung29 <aikepaung767@gmail.com>
2025-09-04 16:30:21 +0700

    update

--- rew-11-route-module/app/routes.ts
+++ rew-11-route-module/app/routes.ts
@@ -1,3 +1,16 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+
+    layout("routes/anonymous/_layout.tsx" , [
+        route("/about", "routes/anonymous/about-us.tsx"),
+        route("/contact", "routes/anonymous/contact.tsx")
+    ]),
+
+    layout("routes/auth/_layout.tsx", [
+        route("/signin", "routes/auth/signin.tsx"),
+        route("/signup", "routes/auth/signup.tsx")
+    ])
+
+] satisfies RouteConfig;
--- rew-11-route-module/app/routes/home.tsx
+++ rew-11-route-module/app/routes/home.tsx
@@ -1,7 +1,4 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +6,7 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>Home</>
+  );
 }
