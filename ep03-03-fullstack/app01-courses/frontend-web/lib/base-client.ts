e325e1cbab6d0ae8e5338139dde90b4c29a581aa
paung29 <aikepaung767@gmail.com>
2025-10-27 15:52:20 +0700

    update

---
+++ ep03-03-fullstack/app01-courses/frontend-web/lib/base-client.ts
@@ -0,0 +1,17 @@
+import "server-only"
+import { RestClientException } from "./types"
+
+export async function request(path:string, init : RequestInit = {}) {
+
+    const endpoint = `${process.env.REST_API}/${path}`
+
+    const response = await fetch(endpoint, init)
+
+    if(!response.ok) {
+        const message = await response.json() as string[]
+        throw new RestClientException(message)
+    }
+
+    return response
+    
+}
